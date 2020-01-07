ab1249bab628d7e2320a1ba1ed3c57bf3af3d9ac
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-01-07 15:58:22 +0000

    Issue #11: Fix unit test, as demo program removed.

--- tests/test_mphy0026.py
+++ tests/test_mphy0026.py
@@ -2,26 +2,8 @@
 
 """MPHY0026 tests"""
 
-from mphy0026.ui.mphy0026_demo import run_demo
-from mphy0026.algorithms import addition, multiplication
 import six
+import pytest
 
-# Pytest style
-
-def test_using_pytest_mphy0026():
-    x = 1
-    y = 2
-    verbose = False
-    multiply = False
-
-    expected_answer = 3
-    assert run_demo(x, y, multiply, verbose) == expected_answer
-
-def test_addition():
-
-    assert addition.add_two_numbers(1, 2) == 3
-
-def test_multiplication():
-
-    assert multiplication.multiply_two_numbers(2, 2) == 4
-
+def test_example_for_later():
+    assert 1 == 1
