cb806422aa492ddde60b593b2819438d3e3cc912
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-02-11 15:18:23 +0000

    Issue #15: Linting.

--- mphy0026/ui/mphy0026_register_app.py
+++ mphy0026/ui/mphy0026_register_app.py
@@ -1,14 +1,19 @@
 # coding=utf-8
 
-""" Harness to run Point/Surface based registration algorithms. """
+""" Harness to run Point/Surface-based registration algorithms. """
 
 import os
 import numpy as np
-import mphy0026.algorithms.registration as reg
 import sksurgerycore.io.load_mps as lmps
+import mphy0026.algorithms.registration as reg
 
 
 def load_file_of_points(file_name):
+    """
+    Loads an MITK .mps file, or .txt file (rows of x y z).
+    :param file_name: string containing path to a valid file
+    :return: Nx3 ndarray
+    """
     extension = os.path.splitext(file_name)[1]
     if extension == '.mps':
         _, points = lmps.load_mps(file_name)
@@ -21,7 +26,8 @@ def run_registration(fixed_points_file,
                      moving_points_file,
                      output_file):
     """
-    Registers point sets, and outputs 4x4 transformations.
+    Registers point sets, optionally saves the 4x4 transform to file
+    and returns the 4x4 transformation and FRE.
 
     :param fixed_points_file: .mps or .txt file of fixed points
     :param moving_points_file: .mps or .txt file of moving points
--- tests/test_register_mps.py
+++ tests/test_register_mps.py
@@ -10,6 +10,9 @@ import mphy0026.ui.mphy0026_register_app as ra
 
 def test_register_pointset_to_self():
 
-    transform, fre = ra.run_registration('tests/data/liver_phantom/ct_landmarks.mps')
+    transform, fre = ra.run_registration('tests/data/liver_phantom/ct_landmarks.mps',
+                                         'tests/data/liver_phantom/ct_landmarks.mps',
+                                         None
+                                         )
     assert np.allclose(transform, np.eye(4))
 
